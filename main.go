b18cfdfab90524ddf3810e68e31ca00ad85b313a
Eren Aydin <nidaynere@gmail.com>
2018-11-08 15:02:30 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//TODO--> Lobby struct. Lobby list.
+///TODO need parser to split to handle sent string
 
 package main
 
@@ -9,29 +9,40 @@ import (
 	"time"
 )
 
-var l net.Listener = nil
+///TCP Listener
+var listener net.Listener = nil
 
+///Server connection info
 const (
     CONN_HOST = "localhost"
     CONN_PORT = "3333"
-    CONN_TYPE = "tcp"
+	CONN_TYPE = "tcp"
 )
 
+///Connected client
 type connection struct {
 	conn net.Conn
 	isConnected bool
+	lobby int // index of the registered lobby.
 }
 
-// Max connections: 16
-var connections [16]connection
+///Lobby
+type lobby struct {
+	connections [8]*connection // A lobby can hold maximum 8 players.
+	name string // Lobby name
+}
+
+/// ALL CONNECTIONS
+var connections [8000]connection // Maximum 8000 players
+var lobbies [1000]lobby // In 1000 lobbies
 
 func addconnection (newconnection net.Conn) {
-	fmt.Println("New connection")
-	for i, conn := range connections {
-		if !conn.isConnected {
+	for i := range connections {
+		if !connections[i].isConnected {
 			connections[i].conn = newconnection
 			connections[i].isConnected = true
-			break;
+			fmt.Println ("Client connected")
+			return
 		}
 	}
 	
@@ -46,27 +57,28 @@ func main() {
         os.Exit(1)
     }
     // Close the listener when the application closes.
-    defer r.Close()
+	defer r.Close()
 	
-	l = r
+	listener = r
 	
     fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 
-	tick := time.Tick(1 * time.Second)
-
+	second := time.Tick(time.Second)
+	millisecond := time.Tick(time.Millisecond)
     for {
         select {
-        case <-tick:
-			go Listen()
-			go ConnectionControl()
-			// -> TODO Message reader.
+        case <-second:
+			go WaitForConnection()
+			go handleRequest()
+		case <-millisecond:
+			go handleMessages()
 		}
-    }
+	}
 }
 
-/// Listen for connections
-func Listen () {
-	conn, err := l.Accept()
+/// Listen for the new connections
+func WaitForConnection () {
+	conn, err := listener.Accept()
 
 	if err != nil {
 		fmt.Println("Error accepting: ", err.Error())
@@ -77,15 +89,10 @@ func Listen () {
 	}
 }
 
-// Handles the connections & drops
-func ConnectionControl() {
+// Handles the connections. Pings them every one second
+func handleRequest() {
 	for i := range connections {
 		if connections[i].isConnected {
-			// Make a buffer to hold incoming data.
-			//buf := make([]byte, 1024)
-			// Read the incoming connection into the buffer.
-			//_, err := conn.conn.Read(buf)
-			
 			fmt.Println("ping to connection")
 			
 			// Send a response back to person contacting us.
@@ -98,4 +105,29 @@ func ConnectionControl() {
 			}
 		}
 	}
+}
+
+func handleMessages () {
+	for i := range connections {
+		if connections[i].isConnected {
+				//Make a buffer to hold incoming data.
+				buf := make([]byte, 1024)
+				//Read the incoming connection into the buffer.
+				length, error := connections[i].conn.Read(buf)
+
+				if error == nil {
+					var incoming string = string(buf[:length])
+					fmt.Println("incoming data: ", incoming) 
+					//fmt.Println("sender's lobby: ", connections[i].lobby)
+					go sendMessage (&connections[i], "mesajini aldim kankey")
+					// TODO: Relay the message to all lobby members
+				}
+		}
+	}			
+}
+
+//usage: sendMessage (&connections[i], incoming) // send back the incoming message
+func sendMessage (conn *connection, msg string) {
+		// Send a response back to person contacting us.
+		conn.conn.Write([]byte(msg))
 }
